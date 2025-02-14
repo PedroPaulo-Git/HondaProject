70c4e28f61f8a3baf6295f0356d0c7ee1071d9f9
Pedro Paulo da Silva Monteiro <109928508+PedroPaulo-Git@users.noreply.github.com>
2025-02-14 10:26:07 -0300

    change <a> to <link> to deploy

--- src/app/components/subheader.tsx
+++ src/app/components/subheader.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 // import { FaPhone } from "react-icons/fa6";
 // import { IoLogoWhatsapp } from "react-icons/io";
@@ -6,7 +7,7 @@ const SubHeaderComponent = () => {
   return (
     <div className=' hidden sm:block'>
         <ul className='text-black flex justify-center gap-14 font-semibold py-4 border-b-2 border-red-600 border-t-gray-300 border-t-[1px]'>
-            <a href="/" className='cursor-pointer text-red-600 hover:text-red-600' > Inicio</a>
+            <Link href="/" className='cursor-pointer text-red-600 hover:text-red-600' > Inicio</Link>
             <a className='cursor-pointer hover:text-red-600'>Contatos</a>
             <a className='cursor-pointer hover:text-red-600'>Novas</a>
             <a className='cursor-pointer hover:text-red-600'>Seminovas</a>
